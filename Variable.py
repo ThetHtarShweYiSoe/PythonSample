ab48df4bd5aa09915c6501eca7b3b860661bbbdc
ThetHtarShweYiSoe <thethtarshweyisoe@gmail.com>
2019-12-23 15:22:48 +0630

    Variable

---
+++ Variable.py
@@ -0,0 +1,48 @@
+#23.12.2019
+#Title - Variable
+
+a = 1
+>>> a
+1
+>>> b = 2
+>>> b
+2
+>>> x = a + b
+>>> x
+3
+>>> text = "Hello"
+>>> text
+'Hello'
+>>> text = "Hello"
+>>> text = Hello
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'Hello' is not defined
+>>> 2 + 2
+4
+>>> 50 - 5 *6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> 8 / 5
+1.6
+>>> 17 / 3
+5.666666666666667
+>>> 17 % 3
+2
+>>> 23 + 20 - 40 % 4 * 2
+43
+>>> 25 % 2 * 3
+3
+>>> 5 * 2
+10
+>>> 5 ** 2
+25
+>>> 6 ** 2
+36
+>>> 2 ** 3
+8
+>>> width = 20
+>>> height = 5 * 9
+>>> width * height
+900
